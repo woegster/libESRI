3793ee5b62470b693f6872323b4597cc18f03941
woegster <tobias.woegerbauer@outlook.com>
2016-07-08 23:42:37 +0200

    init winsock on startup

--- src/libESRI.cpp
+++ src/libESRI.cpp
@@ -6,11 +6,14 @@ namespace libESRI
 {
   IEsriInstance* CreateLibEsriInstance()
   {
+    WSADATA wDat = { 0 };
+    WSAStartup(MAKEWORD(2, 2), &wDat);
     return new EsriInstance();
   }
 
   void DeleteLibEsriInstance(IEsriInstance* instance)
   {
+    WSACleanup();
     delete instance;
   }
 }
