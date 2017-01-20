6a7d886af93d09521fc4daa4bb4137cb4554fa86
woegster <tobias.woegerbauer@outlook.com>
2017-01-20 23:03:27 +0100

    proof of concept terminal functionality

--- src/EsriClientThread.cpp
+++ src/EsriClientThread.cpp
@@ -8,20 +8,22 @@
 
 namespace libESRI
 {
-  bool IsTextOrNumber(const char c)
-  {
-    return c >= 32 && c < 127;
-  }
-
   EsriClientThread::EsriClientThread(toni::TcpClient* tcpClient, EsriHandler* handler)
     : m_tcpClient(tcpClient)
     , m_handler(handler)
+    , m_TelnetLib(nullptr)
   {
     m_InternalHandler = new EsriInternalCommands(handler);
   }
 
   EsriClientThread::~EsriClientThread()
   {
+    if (m_TelnetLib)
+    {
+      telnet_free(m_TelnetLib);
+      m_TelnetLib = nullptr;
+    }
+
     delete m_InternalHandler;
   }
 
@@ -31,7 +33,7 @@ namespace libESRI
     if (welcomeMessage)
     {
       size_t welcomeMessageLength = strlen(welcomeMessage);
-      return m_tcpClient->Send(welcomeMessage, (int)welcomeMessageLength) > 0;
+      telnet_send(m_TelnetLib, welcomeMessage, welcomeMessageLength);
     }
 
     return true;    
@@ -129,47 +131,98 @@ namespace libESRI
       return m_tcpClient->Send(autoCompletedInputWithPrompt.c_str(), (int)autoCompletedInputWithPrompt.length()) > 0;
     }
   }
+
+  void EsriClientThread::OnTelnetEvent(telnet_event_t *ev)
+  {
+    switch (ev->type)
+    {
+      case TELNET_EV_DATA:
+        m_recvBufferAfterTelnet.resize(ev->data.size);
+        memcpy(m_recvBufferAfterTelnet.data(), ev->data.buffer, ev->data.size);
+        break;
+      case TELNET_EV_SEND:
+        m_tcpClient->Send(ev->data.buffer, ev->data.size);
+        break;
+    }
+  }
+
+  int EsriClientThread::OnShellRequiresRead(char* targetBuffer, int bytesToRead)
+  {
+    std::vector<char> rawNetworkData;
+    rawNetworkData.resize(bytesToRead);
+    m_tcpClient->Recv(rawNetworkData.data(), bytesToRead);
+
+    telnet_recv(m_TelnetLib, rawNetworkData.data(), rawNetworkData.size());
+
+    memcpy(targetBuffer, m_recvBufferAfterTelnet.data(), m_recvBufferAfterTelnet.size());
+    m_recvBufferAfterTelnet.clear();
+
+    return bytesToRead;
+  }
+
+  int EsriClientThread::OnShellWantsToWrite(const char* sourceData, int sourceDataSize)
+  {
+    telnet_send(m_TelnetLib, sourceData, sourceDataSize);
+    return sourceDataSize;
+  }
+
+  int EsriClientThread::OnShellCallback(const char* textFromTerminal)
+  {
+    //OutputDebugStringA(textFromTerminal);
+    return 0;
+  }
   
   void EsriClientThread::EntryPoint()
   {
+    auto TelnetEvent_proxy = [](telnet_t *telnet, telnet_event_t *ev, void *user_data)
+    {
+      EsriClientThread* clientThread = static_cast<EsriClientThread*>(user_data);
+      clientThread->OnTelnetEvent(ev);
+    };
+
+    static const telnet_telopt_t telopts[] = {
+      { TELNET_TELOPT_ECHO,	TELNET_WILL, TELNET_DONT }, //TELNET_TELOPT_LINEMODE
+      { TELNET_TELOPT_LINEMODE,	TELNET_WILL, TELNET_DONT },
+      { -1, 0, 0 }
+    };
+
+
+
+    m_TelnetLib = telnet_init(telopts, TelnetEvent_proxy, 0, this);
+
+    auto shell_read_proxy = [](char* buffer, int count, void* userData) -> int
+    {
+      return static_cast<EsriClientThread*>(userData)->OnShellRequiresRead(buffer, count);
+    };
+
+    auto shell_write_proxy = [](const char* buffer, int count, void* userData) -> int
+    {
+      return static_cast<EsriClientThread*>(userData)->OnShellWantsToWrite(buffer, count);
+    };
+
+    auto shell_callback_proxy = [](const char* buffer, void* userData) -> int
+    {
+      return static_cast<EsriClientThread*>(userData)->OnShellCallback(buffer);
+    };
+
+    ntshell_init(&m_nthshell, shell_read_proxy, shell_write_proxy, shell_callback_proxy, this);
+    ntshell_set_prompt(&m_nthshell, "ESRI>");
+
+    ntshell_execute(&m_nthshell);
+
     if (!SendWelcomeMessage())
       return;
-
-    if (!SendCurrentDirectory())
-      return;
+    //
+    //if (!SendCurrentDirectory())
+    //  return;
 
 
     int recvVal = 0;
     std::vector<char> receiveBuffer(1024);
-    std::string inputBuffer;
     while ((recvVal = m_tcpClient->Recv(&receiveBuffer[0], (int)receiveBuffer.size())) > 0)
     {
-      bool sentResponse = false;
-      for (size_t i = 0; i < recvVal && !sentResponse; i++)
-      {
-        const char curChar = receiveBuffer[i];
-        switch (curChar)
-        {
-        case '\t':
-          if (!HandleAutoComplete(inputBuffer))
-            return;
-          inputBuffer.clear();
-          sentResponse = true;
-          break;
-        case '\n':
-          if (!m_InternalHandler->ExecuteInternalCommand(inputBuffer))
-          {
-            m_handler->OnCommitCommand(inputBuffer.c_str());
-          }      
-          inputBuffer.clear();
-          sentResponse = true;
-          break;
-        default:
-          if (IsTextOrNumber(curChar))
-            inputBuffer += curChar;
-          break;
-        }
-      }
+      telnet_recv(m_TelnetLib, receiveBuffer.data(), receiveBuffer.size());
+      memset(receiveBuffer.data(), 0, receiveBuffer.size());
     }
   }
 }
--- src/EsriClientThread.h
+++ src/EsriClientThread.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "TcpClient.h"
+#include "libtelnet.h"
+#include "ntshell.h"
 #include <memory>
 
 namespace libESRI
@@ -18,7 +20,14 @@ namespace libESRI
     bool SendCurrentDirectory();
     bool HandleAutoComplete(std::string& input);
     void AutoComplete(std::string& input, std::string& commonStartOfAllCandidates, std::vector<std::string>& candidates);
+    void OnTelnetEvent(telnet_event_t *ev);
+    int OnShellRequiresRead(char* targetBuffer, int bytesToRead);
+    int OnShellWantsToWrite(const char* sourceData, int sourceDataSize);
+    int OnShellCallback(const char* textFromTerminal);
   private:
+    std::vector<char> m_recvBufferAfterTelnet;
+    ntshell_t m_nthshell;
+    telnet_t* m_TelnetLib;
     toni::TcpClient* m_tcpClient;
     EsriHandler* m_handler;
     EsriInternalCommands* m_InternalHandler;
